168f90c970936c73887a1178c40563169058fecb
gbit <gbitdinger@proton.me>
2023-12-15 10:21:11 -0300

    Finally defined the escope of the game

--- game.rkt
+++ game.rkt
@@ -1,13 +1,13 @@
+#lang racket
+(require rackunit)
+(require rackunit/text-ui)
 
-(define (inquire-user number)
-  (display "Insert a number: ")
-  (define guess (string->number (read-line)))
-  (cond [(> number guess) (displayln "Too low") (inquire-user number)]
-        [(< number guess) (displayln "Too high") (inquire-user number)]
-        [else (displayln "Correct!")]))
+;; String 
+;; Imprime a introdução da história do jogo
+(define (intro)
+  (displayln "--==Essa história se passa em 1645 d.c==--")
+  (displayln "O rei ficou maluco! Ele está chateado com os níveis de educação do país e resolveu intervir da sua própria maneira.")
+  (displayln "Como o rei era adepto do lema 'Programação é como a segunda alfabetização',\ntodas as pessoas seriam submetidas a testes duplos de escrita e de lógica de programação em racket")
+  )
 
-(displayln "Guess a number between 1 and 100")
-(inquire-user (random 100))
-
-(define (par? n) (* 2 n))
-
+;; 
